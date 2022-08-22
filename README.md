604c376cc0f17362045bce4e395fe53fd90d62ae
abhayraihal <b20144@students.iitmandi.ac.in>
2022-08-23 00:07:48 +0530

    changed files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal-Portfolio
-<h2>Go to link : https://abhayraihal.github.io/Personal-Portfolio/</h2>
--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-document.querySelector('.cross').style.display = 'none';
-document.querySelector('.hamburger').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo')
-    if (document.querySelector('.sidebar').classList.contains('sidebarGo')){
-        document.querySelector('.ham').style.display = 'inline'
-        document.querySelector('.cross').style.display = 'none'
-    }else{
-        document.querySelector('.ham').style.display = 'none'
-        setTimeout(()=>{
-        document.querySelector('.cross').style.display = 'inline'
-        }, 300);
-    }
+//---------- Navbar ----------//
+
+window.addEventListener('scroll', function(){
+    let navbar = document.getElementById("navbar");
+    navbar.classList.toggle('fixed', this.window.scrollY > 0)
 })
 
+// Portfolio Gallery
+
+let filterContainer = document.querySelector(".gallery-filter");
+let galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")){
+        // deactivates existing active filter item
+        filterContainer.querySelector(".active").classList.remove("active");
+        // activates new filter item
+        event.target.classList.add("active");
+        let filterValue = event.target.getAttribute("data-filter");
+        galleryItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+
+});
+
+// Review Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1200:{
+            items:2
+        }
+    }
+})
